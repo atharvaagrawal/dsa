598f43b890742289ba54ebb2dc547a659e6875f0
Atharva <atharva7199990@gmail.com>
2021-09-08 18:50:47 +0530

    Initial

---
+++ lco/2-binary-search.py
@@ -0,0 +1,24 @@
+a = [int(x) for x in input().split()]
+
+l = len(a)
+
+v = int(input("Enter value to search: "))
+
+low = 0
+high = l - 1
+mid = (low + high)//2
+
+while(True):
+    if(v==a[mid]):
+        print("value found at:",mid);
+        break;
+    elif(v > a[mid]):
+        low = mid + 1
+        mid = (low + high)//2
+    elif(v <  a[mid]):
+        high = mid - 1
+        mid = (low + high)//2
+    if(low > high):
+        print("Value not present");
+        break;
+
---
+++ tech-interview-pro/1-hashmaps/hash.py
@@ -0,0 +1,10 @@
+hash = {}
+
+hash['a'] = 1 
+hash['b'] = 1 
+
+for key in hash:
+    print(key,hash[key]);
+
+print(hash['a'])
+del hash['a']
