8b93912cfabeb4b26908696228497531cf91f958
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2023-03-07 13:26:13 +0000

    3D DP Question

--- Striver-A2Z/16_Dynamic_Programming/2_2D_3D_DP_and_DP_on_Grids/7_3D_DP_Ninja_and_his_friends.py
+++ Striver-A2Z/16_Dynamic_Programming/2_2D_3D_DP_and_DP_on_Grids/7_3D_DP_Ninja_and_his_friends.py
@@ -26,42 +26,125 @@ Hence the total number of chocolates collected will be 12 + 9 = 21. there is no 
 way to collect a greater number of chocolates than 21.
 '''
 
-# Recursion 
+'''
+# Recursion
 class Solution:
     def solve(self, n, m, grid):
-        
-        def rec(i,j1,j1):
+
+        def rec(i, j1, j2):
             # Base Case
 
-            if j1 < 0 || j2 < 0 || j1 > m || j2 > m:
+            if j1 < 0 or j2 < 0 or j1 >= m or j2 >= m:
                 return -int(1e8)
 
             if i == n-1:
                 if j1 == j2:
                     return grid[i][j1]
                 return grid[i][j1] + grid[i][j2]
-            
-            # Alice and Bob 9 Combination    
+
+            # Alice and Bob 9 Combination
             maxi = 0
 
-            for dj1 in [-1,0,1]:
-                for dj2 in [-1,0,1]:
+            for dj1 in [-1, 0, 1]:
+                for dj2 in [-1, 0, 1]:
                     if j1 == j2:
-                        maxi = max(maxi, grid[i][j1] + rec(i+1,j1+dj1,j2+dj2))
+                        maxi = max(maxi, grid[i][j1] +
+                                   rec(i+1, j1+dj1, j2+dj2))
                     else:
-                        maxi = max(maxi, grid[i][j1]+ grid[i][j2] + rec(i+1,j1+dj1,j2+dj2))
-            
+                        maxi = max(maxi, grid[i][j1] + grid[i]
+                                   [j2] + rec(i+1, j1+dj1, j2+dj2))
+
             return maxi
-                        
-        print( rec(0,0,m-1) )
+
+        print(rec(0, 0, m-1))
+'''
+
+'''
+# Using Memorization
+class Solution:
+    def solve(self, n, m, grid):
+        dp = [[[-1 for i in range(m)] for i in range(m)] for i in range(n)]
+
+        def rec(i, j1, j2):
+            # Base Case
+
+            if j1 < 0 or j2 < 0 or j1 >= m or j2 >= m:
+                return -int(1e8)
+
+            if i == n-1:
+                if j1 == j2:
+                    return grid[i][j1]
+                return grid[i][j1] + grid[i][j2]
+
+            # Alice and Bob 9 Combination
+            maxi = 0
+            if dp[i][j1][j2] != -1:
+                return dp[i][j1][j2]
+
+            for dj1 in [-1, 0, 1]:
+                for dj2 in [-1, 0, 1]:
+                    if j1 == j2:
+                        maxi = max(maxi, grid[i][j1] +
+                                   rec(i+1, j1+dj1, j2+dj2))
+                    else:
+                        maxi = max(maxi, grid[i][j1] + grid[i]
+                                   [j2] + rec(i+1, j1+dj1, j2+dj2))
+
+            dp[i][j1][j2] = maxi
+
+            return maxi
+
+        print(rec(0, 0, m-1))
+'''
+
+# Tablulation
+
+
+class Solution:
+    def solve(self, n, m, grid):
+
+        dp = [[[-1 for i in range(m)] for i in range(m)] for i in range(n)]
+
+        # Base Case
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 == j2:
+                    dp[n-1][j1][j2] = grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]
+
+        for i in range(n-2, -1, -1):
+            for j1 in range(m):
+                for j2 in range(m):
+
+                    maxi = -int(1e8)
+
+                    for dj1 in [-1, 0, 1]:
+                        for dj2 in [-1, 0, 1]:
+                            value = 0
+
+                            if j1 == j2:
+                                value = grid[i][j1]
+                            else:
+                                value = grid[i][j1] + grid[i][j2]
+
+                            if j1+dj1 >= 0 and j1+dj1 < m and j2+dj2 >= 0 and j2+dj2 < m:
+                                value += dp[i+1][j1+dj1][j2+dj2]
+                            else:
+                                value += -int(1e8)
+
+                            maxi = max(maxi, value)
+
+                    dp[i][j1][j2] = maxi
+
+        print(dp[0][0][m-1])
+
 
 r = 3
 c = 4
-grid = [[2, 3, 1, 2], 
+grid = [[2, 3, 1, 2],
         [3, 4, 2, 2],
         [5, 6, 3, 5]]
 
 obj = Solution()
-obj.solve(r,c,grid)
-
-        
+obj.solve(r, c, grid)
